f44ba289e9bf0c6ef796e39b52356ddf4ab51e91
Rosethrn <funfacts1101@gmail.com>
2026-06-26 16:47:00 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,30 +1,33 @@
-const playlist = [
+const songs = [
     "audio/music/song1.mp3",
     "audio/music/song2.mp3",
     "audio/music/song3.mp3"
 ];
 
-let current = 0;
+let currentSong = 0;
 
 const player = document.getElementById("player");
-const status = document.getElementById("status");
 const nowPlaying = document.getElementById("nowPlaying");
 
 function playSong(index) {
-    player.src = playlist[index];
+    player.src = songs[index];
     player.play();
 
-    status.textContent = "Broadcasting";
-    nowPlaying.textContent = playlist[index].split("/").pop();
+    nowPlaying.textContent =
+        "Now Playing: " + songs[index].split("/").pop();
 }
 
+// When a song finishes...
 player.addEventListener("ended", () => {
-    current++;
 
-    if (current >= playlist.length)
-        current = 0;
+    currentSong++;
 
-    playSong(current);
+    if (currentSong >= songs.length) {
+        currentSong = 0;
+    }
+
+    playSong(currentSong);
+
 });
 
-playSong(0);
+playSong(currentSong);
